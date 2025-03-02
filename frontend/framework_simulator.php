6be1e5a0186671b6603c621fbe4a93f83671ed71
John Leo Bruno <brunojohn0004@gmail.com>
2025-03-03 01:56:57 +0800

    add input field, boxes and design.
    improved grid box and input field

--- frontend/framework_simulator.php
+++ frontend/framework_simulator.php
@@ -11,112 +11,155 @@
 </head>
 
 <body>
-    <div class="container overflow-hidden col-md-12" style="max-width: 62rem;">
-        <header>
-            <h3>Rx Entry - Batch Reject - Reject</h3>
-        </header>
-        <div class="pb-1 d-flex justify-content-between mx-auto">
-
-            <div class="">
-                <label>Reorder #</label>
-                <input class="input-field" style="max-width: 5rem;" type="text" value="26647" readonly>
-            </div>
-
-
-            <div class="">
-                <label>Prescription #</label>
-                <input class="input-field" style="max-width: 6rem;" type="text" value="1120607" readonly>
-            </div>
-
-
-            <div class="">
-                <label>Dispensed</label>
-                <input class="input-field" style="max-width: 7rem;" type="text" value="03/22/2025" readonly>
-            </div>
-
-
-            <div class="">
-                <label>Written</label>
-                <input class="input-field" style="max-width: 7rem;" type="text" value="1/14/2024" readonly>
-            </div>
-
-
-            <div class="">
-                <label>Orig</label>
-                <input class="input-field" style="max-width: 7rem;" type="text" value="11/14/2024" readonly>
-            </div>
-
-        </div>
-        <div class="row mx-auto">
-
-            <div class="d-flex flex-column">
-                <button class="rounded-lg text-dark border border-dark" id="button-gradient">Active
-                    Reorders</button>
-                <button class="rounded-lg text-dark border border-dark" id="button-gradient">Add to Profile</button>
-                <button class="rounded-lg text-dark border border-dark" id="button-gradient">New Rx for Pat</button>
-            </div>
-
-            <div class="col">
-                <!-- IF DID NOT FIX JUST REMOVE THE DIV TO RESTORE. -->
-                <div class="row">
-                    <div>
-                        <label>Patient</label>
-                        <input class="input-field" type="text" value="LEO,DAVID">
+    <div class="container overflow-hidden col-md-12" style="max-width: 67rem;">
+        <div>
+            <header>
+                <h3>Rx Entry - Batch Reject - Reject</h3>
+            </header>
+            <div class="row mx-auto justify-content-between">
+                <div class="pb-1 d-flex">
+                    <div class="mr-md-2">
+                        <label>Reorder #</label>
+                        <input class="input-field" style="max-width: 5rem;" type="text" value="26647" readonly>
                     </div>
 
-                    <div>
-                        <button class="input-field rounded-lg text-dark border border-dark" id="button-gradient"
-                            readonly>
-                            <img src="assets/images/play-fill.svg" alt="Patient Profile">
-                        </button>
-                        <button class="input-field rounded-lg text-dark border border-dark" id="button-gradient"
-                            readonly>
-                            <img src="assets/images/hospital.svg" alt="Patient Profile">
-                        </button>
+
+                    <div class="mr-md-2">
+                        <label>Prescription #</label>
+                        <input class="input-field" style="max-width: 6rem;" type="text" value="1120607" readonly>
                     </div>
 
-                    <div>
-                        <label>ID</label>
-                        <input class="input-field" style="max-width: 7rem;" type="text" value="01/31/1958">
+
+                    <div class="mr-md-2">
+                        <label>Dispensed</label>
+                        <input class="input-field" style="max-width: 7rem;" type="text" value="03/22/2025" readonly>
                     </div>
 
-                    <div class="">
-                        <button class="input-field rounded-lg text-dark border border-dark" id="button-gradient"
-                            readonly>Find
-                            ID</button>
-                    </div>
 
-                    <div class="col">
-                        <label>Start</label>
-                        <input class="input-field" style="max-width: 7rem;" type="text" value="01/31/1958">
+                    <div class="mr-md-2">
+                        <label>Written</label>
+                        <input class="input-field" style="max-width: 7rem;" type="text" value="1/14/2024" readonly>
                     </div>
                 </div>
+                <div class=" justify-content-end">
+                    <label>Orig</label>
+                    <input class="input-field" style="max-width: 7rem;" type="text" value="11/14/2024" readonly>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mx-auto">
 
-                <div class="row">
+                <!-- buttons -->
+                <div class="d-flex flex-column">
+                    <button class="rounded-lg text-dark border border-dark" id="button-gradient">Active
+                        Reorders</button>
+                    <button class="rounded-lg text-dark border border-dark" id="button-gradient">Add to Profile</button>
+                    <button class="rounded-lg text-dark border border-dark" id="button-gradient">New Rx for Pat</button>
+                </div>
 
+                <div class="d-flex justify-content-between">
+                    <div class="col flex-column">
+                        <div class="">
+                            <label>Patient</label>
+                            <input class="input-field" style="min-width: 19.7rem;" type="text" value="LEO,DAVID">
+                        </div>
+                        <div class="d-flex">
+                            <div class="mr-md-2" style="max-width: 9rem;">
+                                <label for="">Station</label>
+                                <input class="input-field" style="max-width: 4rem;" type="text" value="Cabsy">
+                            </div>
+                            <div class="mr-md-2" style="max-width: 9rem;">
+                                <label for="">Room</label>
+                                <input class="input-field" style="max-width: 4rem;" type="text" value="Cabsy">
+                            </div>
+                            <div class="mr-md-2" style="max-width: 9rem;">
+                                <label for="">Floor</label>
+                                <input class="input-field" style="max-width: 4rem;" type="text" value="Cabsy">
+                            </div>
+                        </div>
+                        <div class="row pt-5">
+                            <div class="col align-items-end">
+                                <label for="">Last Disp.</label>
+                                <input class="input-field" style="max-width: 7rem;" type="text" value="12/29/1964">
+                            </div>
+                        </div>
+                    </div>
                     <div>
-                        <label>Station</label>
-                        <input class="input-field" type="text" value="Cab">
+                        <div class="col">
+                            <button class="input-field rounded-lg border" id="button-gradient" readonly>
+                                <img src="assets/images/play-button.png" style="height: 15px" alt="Patient Profile">
+                            </button>
+
+                            <button class="input-field rounded-lg border" id="button-gradient" readonly>
+                                <img src="assets/images/hospital-button.png" style="height: 15px" alt="Patient Profile">
+                            </button>
+
+                            <label>ID</label>
+                            <input class="input-field" style="max-width: 7rem;" type="text" value="01/31/1958">
+
+                            <button class="input-field rounded-lg border text-black-50 border-white"
+                                style="cursor: no-drop" id="button-gradient" readonly>Find
+                                ID</button>
+                        </div>
+                        <div class="col">
+                            <label>Sex</label>
+                            <input class="input-field" style="max-width: 2rem;" type="text" value="F">
+
+                            <label>DOB</label>
+                            <input class="input-field" type="text" style="max-width: 7rem;" value="12/29/1964">
+
+                            <button class="input-field rounded-lg border" id="button-gradient" readonly>
+                                <img src="assets/images/docs.png" style="height: 15px" alt="Patient Profile">
+                            </button>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>Room</label>
-                        <input class="input-field" type="text" value="304">
+                    <div class="flex-column">
+                        <div class="justify-between-end">
+                            <label>Start</label>
+                            <input class="input-field" style="max-width: 7rem;" type="text" value="11/14/2024" readonly>
+                        </div>
+                        <div class="">
+                            <label>Available Payment</label>
+                            <div class="border border-dark ">
+                                <ul class="col" style="list-style-type: none;">
+                                    <li class="border-bottom border-white"><input type="checkbox" checked disabled> ADV
+                                        1
+                                    </li>
+                                    <li class="border-bottom border-white"><input type="checkbox" disabled> HMRK</li>
+                                    <li class="border-bottom border-white"><input type="checkbox" disabled> FACI</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pt-2 row mx-auto justify-content-between">
+                <div class="pb-1 d-flex">
+                    <div class="mr-md-2">
+                        <label>Prescriber</label>
+                        <input class="input-field" type="text" value="26647" readonly>
+
+                        <button class="input-field rounded-lg border" id="button-gradient" readonly>
+                            <img src="assets/images/play-button.png" style="height: 15px" alt="Patient Profile">
+                        </button>
+
                     </div>
 
-                    <div>
-                        <label>Floor</label>
-                        <input class="input-field" type="text" value="3">
+                    <div class="mr-md-2">
+                        <label>DEA #</label>
+                        <input class="input-field" style="max-width: 6rem;" type="text" value="1120607" readonly>
                     </div>
 
-                    <div>
-                        <label>Sex</label>
-                        <input class="input-field" type="text" value="F">
+
+                    <div class="mr-md-2">
+                        <label>NPI #</label>
+                        <input class="input-field" style="max-width: 7rem;" type="text" value="03/22/2025" readonly>
                     </div>
 
-                    <div>
-                        <label>DOB</label>
-                        <input class="input-field" type="text" value="12/29/1964">
+
+                    <div class="mr-md-2">
+                        <label>SPI</label>
+                        <input class="input-field" type="text" value="1/14/2024" readonly>
                     </div>
                 </div>
             </div>
