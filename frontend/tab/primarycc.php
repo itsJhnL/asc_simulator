df77210ce35d5b30d6740dd41d12df38a496082a
John Leo Bruno <107639845+itsJhnL@users.noreply.github.com>
2025-03-21 21:53:49 +0800

    added primar
    added primary tab

--- frontend/tab/primarycc.php
+++ frontend/tab/primarycc.php
@@ -8,6 +8,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+/* pull this */
 
 $clarificationCode = isset($_SESSION["clarificationCode"]) ? $_SESSION["clarificationCode"] : "No clarification code saved.";
 ?>
